4cbb7cd2490f45c7b611832f53cc7625bebe016e
Kristen Studioso <45612650+studiosok@users.noreply.github.com>
2025-06-03 12:57:03 -0400

    error alerts and fe error handling

--- public/site.js
+++ public/site.js
@@ -1,27 +1,32 @@
-// TODO: error handling and try/catch
-
 const submitText = async () => {
     const data = await document.getElementById("ingredients").value;
 
     const jsonData = JSON.stringify({data: data})
-    
-    await fetch('/search', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "origin"
-            },
-            body: jsonData
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then((response) => {
-            populateResults(response);
-        })
+    try {
+        await fetch('/search', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "origin"
+                },
+                body: jsonData
+            })
+            .then((response) => {
+                return response.json();
+            }, (error) => {
+                throw new Error(error.message || 'Error searching data')
+            })
+            .then((response) => {
+                populateResults(response);
+            })
+        }
+    catch(error) {
+        alert(error.message || 'Unspecified error')
+    }
     }
 
 const populateResults = async (response) => {
+    try {
     // clear input box
     document.getElementById('ingredients').value = '';
     
@@ -42,6 +47,7 @@ const populateResults = async (response) => {
         for (i = 0; i < allergens.length; i++) {
             let li = document.createElement('li');
             li.innerText = allergens[i];
+            li.className = 'danger';
             list.appendChild(li);
         }
     };
@@ -51,7 +57,12 @@ const populateResults = async (response) => {
         for (i = 0; i < risks.length; i++) {
             let li = document.createElement('li');
             li.innerText = `${risks[i][0]} may be related to ${risks[i][1]}`;
+            li.className = 'warn';
             list.appendChild(li);
         }
+    }
+    }
+    catch(error) {
+        throw new Error(error.message || 'Error populating results to HTML')
     }
 }
